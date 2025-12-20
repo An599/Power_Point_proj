7f25c9b7c3bdb12ec95625c130fe836c3d1156d4
Anush Yesayan <70717954+An599@users.noreply.github.com>
2025-12-20 19:11:23 +0400

    Update IDeserialize.h

--- Serialization/IDeserialize.h
+++ Serialization/IDeserialize.h
@@ -4,15 +4,11 @@
 #include <string>
 
 namespace Serialization {
-
-    // IDeserialize - interface for deserializing presentations from files
     class IDeserialize {
     public:
         virtual ~IDeserialize() = default;
-
-        // Load presentation from a file
         virtual std::unique_ptr<Model::Presentation> load(const std::string& filename) const = 0;
     };
 
-} // namespace Serialization
+} 
 
